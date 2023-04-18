630017635de9e3bb9fde5182f1815fdaf332aeae
Michael Ekstrand <md@ekstrandom.net>
2023-04-18 17:32:40 +0000

    works?

--- deploy-to-netlify.go
+++ deploy-to-netlify.go
@@ -1,20 +1,35 @@
 package main
 
 import (
-	"context"
+	goctx "context"
+	"errors"
 	"flag"
 	"fmt"
 	"os"
 
+	"github.com/go-openapi/runtime"
+	"github.com/go-openapi/strfmt"
 	"github.com/netlify/open-api/v2/go/porcelain"
+	"github.com/netlify/open-api/v2/go/porcelain/context"
 )
 
 var site_path = flag.String("d", "_site", "directory from which to deploy the site")
 
+func add_auth_token(req runtime.ClientRequest, reg strfmt.Registry) error {
+	token, found := os.LookupEnv("NETLIFY_AUTH_TOKEN")
+	if !found {
+		return errors.New("no authentication token")
+	}
+	req.SetHeaderParam("Authorization", fmt.Sprintf("Berarer %s", token))
+	return nil
+}
+
 func main() {
 	flag.Parse()
 
-	ctx := context.TODO()
+	auth_info := runtime.ClientAuthInfoWriterFunc(add_auth_token)
+	ctx := context.WithAuthInfo(goctx.TODO(), auth_info)
+
 	n := porcelain.Default
 	var deploy_req porcelain.DeployOptions
 	deploy_req.SiteID = os.Getenv("NETLIFY_SITE_ID")
